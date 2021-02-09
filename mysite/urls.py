b0a259caf4955d381b5726673e1870b7ae1ad988
randalhsu <randalaaaa@gmail.com>
2021-02-09 22:50:23 +0800

    Add favicon

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', include('ChartExerciser.urls')),
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(
+        url=staticfiles_storage.url('images/favicon.ico'))),
 ]
